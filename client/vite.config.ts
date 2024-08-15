278d87cbdfb242186c21cff8f959c3b5db176a0c
Damián Bastos <damian.bastos@yahoo.com>
2024-08-15 02:30:45 -0300

    configuracion deploy front

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Configura Vite para escuchar en todas las interfaces de red
+    port: 3000, // Puedes cambiar el puerto si es necesario
+  },
 })
