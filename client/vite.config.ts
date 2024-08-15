1366f662516e647624c3462331caa98f0953820f
Damián Bastos <damian.bastos@yahoo.com>
2024-08-15 02:31:51 -0300

    cambio error puerto front

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Configura Vite para escuchar en todas las interfaces de red
-    port: 3000, // Puedes cambiar el puerto si es necesario
   },
 })
